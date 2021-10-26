56d869f0fa1a9d160f2cb8f97a18efd17dc37adc
Abiodun Adefusi <90515100+King-Abbey@users.noreply.github.com>
2021-10-26 12:40:32 +0100

    Add files via upload

---
+++ Basic Python.py
@@ -0,0 +1,12 @@
+from math import sqrt
+from math import factorial
+
+print("Hello World!")  # this print this
+x = 15 * 5
+y = 40
+#  this will print both variable x and y
+print(x, y)
+# Square root
+number = 20
+print(sqrt(number))
+print(factorial(50))
